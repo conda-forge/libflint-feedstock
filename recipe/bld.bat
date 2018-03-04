9bf66f4eb925cafbdacda5cbd07da091dc3cb301
xoviat <xoviat@users.noreply.github.com>
2018-03-04 16:20:29 -0600

    List directory

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,16 @@
 :: Configure.
+
+dir
+
+mkdir build
+cd build
+
 cmake -G "Ninja" ^
       -D CMAKE_BUILD_TYPE=Release ^
+      -D BUILD_SHARED_LIBS=ON ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
-      %SRC_DIR%
+      ..
 if errorlevel 1 exit 1
 
 :: Build.
