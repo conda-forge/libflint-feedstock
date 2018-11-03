9cb585130188bd48fc64f284ae94f87006f925f5
Isuru Fernando <isuruf@gmail.com>
2018-11-02 19:14:42 -0500

    Add C++11 flags for building with NTL

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,10 @@ chmod +x configure
 
 if [[ "$cxx_compiler" == "toolchain_cxx" ]]; then
     export CFLAGS="$CFLAGS -ansi -pedantic -Wall -O2 -funroll-loops -g -mpopcnt -Wno-unknown-pragmas"
+    export CXXFLAGS="$CXXFLAGS -std=c++11 -pedantic -Wall -O2 -funroll-loops -g -mpopcnt -Wno-unknown-pragmas"
+else
+    export CFLAGS="$CFLAGS -funroll-loops -g -mpopcnt -Wno-unknown-pragmas"
+    export CXXFLAGS="$CXXFLAGS -funroll-loops -g -mpopcnt -Wno-unknown-pragmas"
 fi
 
 # This is set to reduce the number of random tests run so that CIs can run 
