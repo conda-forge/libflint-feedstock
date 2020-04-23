fbd2618af68fa87b83974c802318434a89a3f08b
Isuru Fernando <isuruf@gmail.com>
2020-04-23 00:45:31 +0000

    fix typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,10 +5,10 @@ chmod +x configure
 export CFLAGS="$CFLAGS -funroll-loops -g -Wno-unknown-pragmas"
 export CXXFLAGS="$CXXFLAGS -funroll-loops -g -Wno-unknown-pragmas"
 
-if [[ "$target-platform" == *-64 ]]; then
+#if [[ "$target-platform" == *-64 ]]; then
     #export CFLAGS="$CFLAGS -mpopcnt"
     #export CXXFLAGS="$CXXFLAGS -mpopcnt"
-fi
+#fi
 
 # This is set to reduce the number of random tests run so that CIs can run 
 # tests to completion without timeouts.
@@ -17,5 +17,5 @@ echo "int flint_test_multiplier(){return 1;}" > test_helpers.c
 ./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX --with-ntl=$PREFIX --disable-static
 make -j${CPU_COUNT}
 make install
-ls -al ${PREFIX}/lib
+#ls -al ${PREFIX}/lib
 make check
