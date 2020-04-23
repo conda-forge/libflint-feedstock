daf758c6e10501baaa1827377cb0df58e600867b
Isuru Fernando <isuruf@gmail.com>
2020-04-23 00:39:18 +0000

    Fix win build

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,15 +6,15 @@ export CFLAGS="$CFLAGS -funroll-loops -g -Wno-unknown-pragmas"
 export CXXFLAGS="$CXXFLAGS -funroll-loops -g -Wno-unknown-pragmas"
 
 if [[ "$target-platform" == *-64 ]]; then
-    export CFLAGS="$CFLAGS -mpopcnt"
-    export CXXFLAGS="$CXXFLAGS -mpopcnt"
+    #export CFLAGS="$CFLAGS -mpopcnt"
+    #export CXXFLAGS="$CXXFLAGS -mpopcnt"
 fi
 
 # This is set to reduce the number of random tests run so that CIs can run 
 # tests to completion without timeouts.
 echo "int flint_test_multiplier(){return 1;}" > test_helpers.c
 
-./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX --with-ntl=$PREFIX
+./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX --with-ntl=$PREFIX --disable-static
 make -j${CPU_COUNT}
 make install
 ls -al ${PREFIX}/lib
