b2de078c23293dc8f72727b3bcf5f980a5414ba9
Julian Rüth <julian.rueth@fsfe.org>
2024-08-03 22:26:56 +0200

    Pass --disable-arch as recommended in 'https://github.com/conda-forge/libflint-feedstock/pull/32#issuecomment-2255561465'

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,19 +5,14 @@ chmod +x configure
 export CFLAGS="$CFLAGS -funroll-loops -g -Wno-unknown-pragmas"
 export CXXFLAGS="$CXXFLAGS -funroll-loops -g -Wno-unknown-pragmas"
 
-#if [[ "$target-platform" == *-64 ]]; then
-    #export CFLAGS="$CFLAGS -mpopcnt"
-    #export CXXFLAGS="$CXXFLAGS -mpopcnt"
-#fi
-
 # This is set to reduce the number of random tests run so that CIs can run 
 # tests to completion without timeouts.
 echo "int flint_test_multiplier(){return 1;}" > test_helpers.c
 
 if [[ "$WITH_NTL" == 1 ]]; then
-  ./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX --with-ntl=$PREFIX --disable-static
+  ./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX --with-ntl=$PREFIX --disable-static --disable-arch
 else
-  ./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX --disable-static
+  ./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX --disable-static --disable-arch
 fi
 
 make -j${CPU_COUNT}
