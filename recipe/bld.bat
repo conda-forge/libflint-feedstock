0561c09a6805c5958fe1a10599a62275e9f27bac
Isuru Fernando <isuruf@gmail.com>
2024-08-25 18:36:18 -0500

    enable windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,19 @@
-call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
-if %ERRORLEVEL% neq 0 exit 1
+REM reduce the number of tests
+echo int flint_test_multiplier(){return 1;} > test_helpers.c
+
+mkdir build
+cd build
+
+cmake ^
+  -G "Ninja" ^
+  -DBUILD_TESTING=ON ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+  ..
+
+ninja -j%CPU_COUNT%
+
+ctest -j%CPU_COUNT% --verbose --timeout 600
+
+ninja install
